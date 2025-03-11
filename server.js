204a76e6e4e0671fa305cdd51cf1e3be699b88fb
VelesDragon <ScorpionEvil777@gmai.com>
2025-03-11 13:04:51 +0300

    Реализация простенького API на ноде

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const http = require('http')
 const path = require('path')
 const fs = require('fs')
-const PORT = 3001
+const PORT = 3000
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8',
-    })
-
     if (req.url === '/') {
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8',
+      })
+
       fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (err, content) => {
         if (err) {
           throw err
@@ -18,6 +18,10 @@ const server = http.createServer((req, res) => {
         res.end(content)
       })
     } else if (req.url === '/about') {
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8',
+      })
+
       fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (err, content) => {
         if (err) {
           throw err
@@ -25,22 +29,39 @@ const server = http.createServer((req, res) => {
 
         res.end(content)
       })
+    } else if (req.url === '/api/users') {
+      console.log('Работает')
+
+      res.writeHead(200, {
+        'Content-Type': 'text/json',
+      })
+      const users = [
+        { name: 'Veles', age: 42 },
+        { name: 'Olga', age: 35 },
+      ]
+      res.end(JSON.stringify(users))
     }
   } else if (req.method === 'POST') {
+    // Массив для хранения данных
     const body = []
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8',
     })
 
+    // Получаем данные из буфера обмена
     req.on('data', (data) => {
       body.push(Buffer.from(data))
     })
 
+    // Проверка на получени данных
     req.on('end', () => {
+      // Преобразовываем данные из формата buffer к формату строки, разделяем по знаку = и получаем только данные
       const message = body.toString().split('=')[1]
-      console.log(body.toString().split('=')[1])
 
-      res.end(`<h1>Ваше сообщение ${message}</h1>`)
+      // После того как завершились операции мы можем вывести их на страницу
+      res.end(`
+          <h1>Ваше сообщение: ${message} </h1>
+          `)
     })
   }
 })
