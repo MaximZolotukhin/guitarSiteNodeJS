fcfccff951462e2ceadf035201ab4ef145987ac7
VelesDragon <ScorpionEvil777@gmai.com>
2025-03-11 19:35:26 +0300

    Добавил шаблоны для страницы

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 // Подключение express
 const express = require('express')
 const app = express()
+const path = require('path')
 
 // Объявляем порт
 const PORT = process.env.PORT | 3000
 
+app.get('/', (req, res) => {
+  res.status(200)
+
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.status(200)
+
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server start on port ${PORT}`)
 })
