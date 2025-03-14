96d293ac721c60ecd76b51356f91d96164658f4f
VelesDragon <ScorpionEvil777@gmai.com>
2025-03-14 13:48:08 +0300

    Добавил отображение каталога товаров

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,10 +1,13 @@
 const { Router } = require('express')
 const router = Router()
+const Products = require('../models/products')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const products = await Products.getAll()
   res.render('catalog', {
     title: 'Католог товаров',
     isCatalog: true,
+    products,
   })
 })
 
