5c74545e536b1671276d8b1cb96e58aca4c1b33c
VelesDragon <ScorpionEvil777@gmai.com>
2025-03-31 18:53:02 +0300

    Добавил модель юзеров

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose')
+
+const userShema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        count: {
+          type: Nember,
+          required: true,
+          default: 1,
+        },
+        coursesId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+      },
+    ],
+  },
+})
+
+module.exports = model('User', userShema)
