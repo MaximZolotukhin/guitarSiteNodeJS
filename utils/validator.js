af34a89799e34ce8dc2bbb2f59ac853134f4c9e6
VelesDragon <ScorpionEvil777@gmai.com>
2025-05-02 13:52:40 +0300

    Настройка санитайзеров для полей  ipnput формы регистрации

--- utils/validator.js
+++ utils/validator.js
@@ -14,14 +14,27 @@ exports.registerValidators = [
       } catch (error) {
         console.log(error)
       }
-    }),
-  body('password', 'Пароль должне быть минимум 6 символов').isLength({ min: 6, max: 56 }).isAlphanumeric(),
+    })
+    //Урок 4
+    .normalizeEmail(),
+  body('password', 'Пароль должне быть минимум 6 символов')
+    .isLength({ min: 6, max: 56 })
+    .isAlphanumeric()
+    // Урок 4
+    .trim(),
   // Кастомный валидатор
-  body('confirm').custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error('Пароли должны совпадать')
-    }
-    return true
-  }),
-  body('name').isLength({ min: 3 }).withMessage('Имя должно быть минимум 3 символа'),
+  body('confirm')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Пароли должны совпадать')
+      }
+      return true
+    })
+    // Урок 4
+    .trim(),
+  body('name')
+    .isLength({ min: 3 })
+    .withMessage('Имя должно быть минимум 3 символа')
+    // Урок 4
+    .trim(),
 ]
